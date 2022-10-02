d9e3093090f2445fb1acfbfcf9effc09c3073e61
Nick Fox-Gieg <nick@fox-gieg.com>
2022-10-02 13:10:08 -0400

    shell

---
+++ ubuntu/run.sh
@@ -0,0 +1,1 @@
+jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' --port=9090 --no-browser
---
+++ ubuntu/setup.sh
@@ -0,0 +1,6 @@
+pip install markupsafe==2.0.1
+
+pip install jupyter
+pip install jupyter_http_over_ws
+jupyter serverextension enable --py jupyter_http_over_ws
+
