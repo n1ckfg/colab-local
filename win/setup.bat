36aec46219afd6acf44eb619fc2691599816faa1
Nick Fox-Gieg <nick@fox-gieg.com>
2022-10-02 21:56:31 -0400

    Update setup.bat

--- win/setup.bat
+++ win/setup.bat
@@ -4,4 +4,6 @@ pip install jupyter
 pip install jupyter_http_over_ws
 jupyter serverextension enable --py jupyter_http_over_ws
 
+pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu113/torch_nightly.html
+
 @pause
